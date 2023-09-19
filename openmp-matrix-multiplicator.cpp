a4ca343b9e867c3fcc650dfc921ce1e25a983f40
otusnoctua <raymantishq@gmail.com>
2023-09-19 17:12:30 +0300

    Add project files.

---
+++ openmp-matrix-multiplicator.cpp
@@ -0,0 +1,12 @@
+﻿// openmp-matrix-multiplicator.cpp : Defines the entry point for the application.
+//
+
+#include "openmp-matrix-multiplicator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
